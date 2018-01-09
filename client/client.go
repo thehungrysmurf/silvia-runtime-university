badf4616a37bfbab92cf573a3e99236a31f263c7
Silvia Gheorghita <sgheorghita@salesforce.com>
2018-01-09 12:23:22 -0800

    Implement GetFeatures client method

---
+++ client/client.go
@@ -0,0 +1,23 @@
+package client
+
+import (
+	"golang.org/x/net/context"
+	"github.com/heroku/silvia-runtime-university/spec"
+)
+
+type RouteGuide struct {
+	client spec.RouteGuideClient
+}
+
+func (rg *RouteGuide) GetFeatures(ctx context.Context, points []spec.Point) ([]spec.Feature, error) {
+	var features []spec.Feature
+	for _, point := range points {
+		feature, err := rg.client.GetFeature(ctx, &point)
+		if err != nil {
+			return nil, err
+		}
+		features = append(features, *feature)
+	}
+
+	return features, nil
+}
